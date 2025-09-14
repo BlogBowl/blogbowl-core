431c470979aa7ae5e59f1569d4653ccee5b986ba
Daniil Poletaev <44584010+danpoletaev@users.noreply.github.com>
2025-09-14 12:11:23 +0200

    chore

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,6 +2,7 @@ Sidekiq.configure_server do |config|
   config.redis = Rails.application.config_for('redis/shared')
   config.queues = %w[default newsletter postmark_webhooks journey]
 
+  # TODO: PRO
   # Sidekiq::Cron::Job.load_from_hash YAML.load_file("config/schedule.yml")
 end
 
