1572e1d821c8258f2fe8e8a206573ea8840ad119
Daniil Poletaev <44584010+danpoletaev@users.noreply.github.com>
2025-09-21 12:07:13 +0200

    chore: api internal application controller to concern

--- app/controllers/api/internal/application_controller.rb
+++ app/controllers/api/internal/application_controller.rb
@@ -1,22 +1,3 @@
 class API::Internal::ApplicationController < ApplicationController
-  skip_forgery_protection if Rails.env.development?
-
-  rescue_from CanCan::AccessDenied do
-    render json nothing: true, status: :not_found
-  end
-
-  def authenticate
-    session_record = Session.find_by_id(cookies.signed[:session_token])
-    if session_record.present?
-      Current.session = session_record
-      # TODO: PRO
-      # if session_record.user.verified?
-      #   Current.session = session_record
-      # else
-      #   render json: { error: "Unverified email" }, status: :precondition_required
-      # end
-      return
-    end
-    render json: { errors: "Unauthorized" }, status: :unauthorized
-  end
+  include API::Internal::ApplicationControllerConcern
 end
