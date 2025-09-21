1485550f8d11a52e02d7c15c0edb37da9bd5cccd
Daniil Poletaev <44584010+danpoletaev@users.noreply.github.com>
2025-09-21 11:53:20 +0200

    clean: workspace setting concern

--- app/models/concerns/models/workspace_setting_concern.rb
+++ app/models/concerns/models/workspace_setting_concern.rb
@@ -4,18 +4,5 @@ module Models::WorkspaceSettingConcern
   included do
     belongs_to :workspace
     accepts_nested_attributes_for :workspace, update_only: true
-
-    # TODO: PRO
-    # validate :free_plan_has_watermark
   end
-  #
-  # private
-  #
-  # def free_plan_has_watermark
-  #   if workspace.free? && !with_watermark?
-  #     errors.add(:base, "Watermark can't be removed from free plan")
-  #   end
-  # end
-
-
 end
