8a9f8f99a50a3caf88788fe01640e0a7a1c64312
Daniil Poletaev <44584010+danpoletaev@users.noreply.github.com>
2025-09-21 13:00:44 +0200

    chore: clean up views

--- app/mailers/newsletter_test_mailer.rb
+++ app/mailers/newsletter_test_mailer.rb
@@ -1,15 +1,3 @@
 class NewsletterTestMailer < ApplicationMailer
   include NewsletterTestMailerConcern
-  # TODO: PRO
-  # after_action :secondary_delivery, only: [:send_test_email]
-
-  # def send_test_email(email, newsletter_settings, email_address)
-  #   @email = email
-  #   @settings = newsletter_settings
-  #
-  #   mail(
-  #     to: email_address,
-  #     subject: "[TEST E-MAIL] #{email.subject}"
-  #   )
-  # end
 end
