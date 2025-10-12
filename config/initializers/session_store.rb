b03885130c4d6720328ed73cb29446763cd4cf5f
Daniil Poletaev <44584010+danpoletaev@users.noreply.github.com>
2025-10-12 18:34:10 +0200

    fix: session

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,1 @@
-cookie_domain = ['', 'analytics.'].map do |subdomain|
-  subdomain + Rails.application.routes.default_url_options[:host]
-end
-
-Rails.application.config.session_store :cookie_store,
-                                       key: '_blogbowl_session',
-                                       domain: cookie_domain
+Rails.application.config.session_store :cookie_store, key: '_blogbowl_session', same_site: :lax
