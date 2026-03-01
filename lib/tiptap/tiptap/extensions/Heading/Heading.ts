39349f1e58417ea187e326dfcf979ed826546aab
Daniil Poletaev <44584010+danpoletaev@users.noreply.github.com>
2026-03-01 17:36:56 +0100

    fix: double fix

--- lib/tiptap/tiptap/extensions/Heading/Heading.ts
+++ lib/tiptap/tiptap/extensions/Heading/Heading.ts
@@ -3,6 +3,20 @@ import TiptapHeading from '@tiptap/extension-heading'
 import type { Level } from '@tiptap/extension-heading'
 
 export const Heading = TiptapHeading.extend({
+  addAttributes() {
+    return {
+      ...this.parent?.(),
+      id: {
+        default: null,
+        parseHTML: (element) => element.getAttribute('id') || null,
+        renderHTML: (attributes) => {
+          if (!attributes.id) return {}
+          return { id: attributes.id }
+        },
+      },
+    }
+  },
+
   renderHTML({ node, HTMLAttributes }) {
     const nodeLevel = parseInt(node.attrs.level, 10) as Level
     const hasLevel = this.options.levels.includes(nodeLevel)
