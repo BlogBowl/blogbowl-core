2118acc5375d5a7aee4d15afcce265d309e6bc1f
Daniil Poletaev <danpoletaev1@gmail.com>
2026-07-15 20:20:20 +0200

    refactor: clean up

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -3,7 +3,7 @@ Apipie.configure do |config|
   config.api_base_url            = "/api/v1"
   config.doc_base_url            = "/apidoc"
   config.app_info                = "REST API for managing blog pages, posts, authors, newsletters, and subscribers."
-  config.api_controllers_matcher = [Core::Engine.root.join("app/controllers/api/v1/**/*.rb").to_s]
+  config.api_controllers_matcher = [ Core::Engine.root.join("app/controllers/api/v1/**/*.rb").to_s ]
   config.validate                = false
   config.generator.swagger.content_type_input = :json
 end
